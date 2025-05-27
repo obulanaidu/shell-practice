5c203600ad47d568ff89824bb78ff41ffbd5d72d
obulanaidu <obulnaiduganaparty@gmail.com>
2025-05-27 21:38:33 +0530

    hello world started

--- variables.sh
+++ variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-person1=ravi
-person2=raju
+person1=naveen
+person2=rajesh
 
 echo "$person1 : : Hi $person2, How are you?"
 echo "$person2 : : Hi $person1, I am Fine,How are you doing?"
