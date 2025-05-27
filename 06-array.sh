7a67855f2fd4da9ed74b9751f9c10e95873c217f
obulanaidu <obulnaiduganaparty@gmail.com>
2025-05-27 23:46:15 +0530

    hello world started

--- 06-array.sh
+++ 06-array.sh
@@ -4,6 +4,7 @@ MOVIES=("Court" "HIT3" "PUSHPA3" "Thandel")
 
 echo "First Movie: ${MOVIES[0]}"
 echo "First Movie: ${MOVIES[3]}"
+echo "First Movie: ${MOVIES[4]}"
 
 echo "All movies: ${MOVIES[@]}"
 
