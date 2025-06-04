b8df90756f28b8162614cd48ea8c8e4ad0d07701
obulanaidu <obulnaiduganaparty@gmail.com>
2025-06-04 21:15:34 +0530

    ansible

--- 07-special-vars.sh
+++ 07-special-vars.sh
@@ -2,3 +2,10 @@
 
 echo "all variables passed in script: $@"
 echo "no variables passed in script: $#"
+echo "name of the script: $0"
+echo "present working directory: $PWD"
+echo "home directory of the user who is running script: $home"
+echo "User who is running the script: $USER"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of the last working command in background: $!"
