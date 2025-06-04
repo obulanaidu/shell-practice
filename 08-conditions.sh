ffeafd082d805b5dbe8a6a33412a37d41ef838e9
obulanaidu <obulnaiduganaparty@gmail.com>
2025-06-04 21:51:16 +0530

    ansible

---
+++ 08-conditions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ number -lt 10 ]
+then
+    echo "given number $NUMBER is less than 10"
+else
+    echo "given number $NUMBER is not less than 10"
+fi
+
+
+# -gt=greater gthan
+# -lt=less than
+# -eq=equal
+# -ne=not equal
