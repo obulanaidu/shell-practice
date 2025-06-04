00c4621ef9e42fc6b8ff0561e41c9e14d233ef83
obulanaidu <obulnaiduganaparty@gmail.com>
2025-06-04 21:08:42 +0530

    ansible

--- 07-special-vars.sh
+++ 07-special-vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo " all variables passed in script: $@
-echo " no variables passed in script: $#
+echo "all variables passed in script: $@"
+echo "no variables passed in script: $#"
