c3b229f046ae0ca884aca8b22afea721a93d3768
obulanaidu <obulnaiduganaparty@gmail.com>
2025-06-04 22:18:15 +0530

    ansible

--- 09-install-command.sh
+++ 09-install-command.sh
@@ -4,7 +4,9 @@ uSERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "error:: please run the user with root access"
+    echo "error:: please run the script with root access"
 else
     echo " you are running with root acess"
 fi
+
+dnf install mysql -y
