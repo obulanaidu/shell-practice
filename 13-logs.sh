a32b4a5823b8738fe3700ac4459599212c63dc07
obulanaidu <obulnaiduganaparty@gmail.com>
2025-06-06 22:07:50 +0530

    colors

--- 13-logs.sh
+++ 13-logs.sh
@@ -11,7 +11,7 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "script started executing at: $(date)" tee -a $LOG_FILE
+echo "script started executing at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
