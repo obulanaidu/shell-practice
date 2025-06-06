23f0e798aaffa35273194fdce1324252df2dd884
obulanaidu <obulnaiduganaparty@gmail.com>
2025-06-06 23:08:02 +0530

    colors

--- 15-loops.sh
+++ 15-loops.sh
@@ -16,18 +16,18 @@ echo "script started executing at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "give $R ERROR and run the script with root access $N" tee -a $LOG_FILE
+    echo -e "give $R ERROR and run the script with root access $N" | tee -a $LOG_FILE
     exit 1 # give other than 0 up to 127
 else
-    echo "you are running with root acces" tee -a $LOG_FILE
+    echo "you are running with root acces" | tee -a $LOG_FILE
 fi
 # validate takes input as exit status,what the command to install
 VALIDATE(){
     if [ $1 -eq 0 ] 
     then
-        echo -e "$2 installation is $G success $N" tee -a $LOG_FILE
+        echo -e "$2 installation is $G success $N" | tee -a $LOG_FILE
     else
-        echo -e "give $R ERROR $2 is not installed $N" tee -a $LOG_FILE
+        echo -e "give $R ERROR $2 is not installed $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -36,11 +36,11 @@ do
     dnf list installed $package &>>$LOG_FILE
     if [ $? -ne 0 ]
     then
-        echo "give error ..$package is going to install" tee -a $LOG_FILE
+        echo "give error ..$package is going to install" | tee -a $LOG_FILE
         dnf install $package -y &>>$LOG_FILE
         VALIDATE $? $package
     else
-        echo -e $package is already $Y installed nothing to do $N" tee -a $LOG_FILE
+        echo -e "nothing to do...$package...$Y already installed $N" | tee -a $LOG_FILE
     fi
 done
 
